ceea5cf98680679fe86179773a3b4bc60b279783
Sanket Singh <singhsanket143@gmail.com>
2025-01-10 14:39:27 +0530

    Added codes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import { PLayerLayout } from './Layouts/PlayerLayout'
+import { Home } from './components/pages/Home'
+
+function App() {
+  return (
+    <>
+      <Routes>
+      <Route path="/" element={<PLayerLayout><Home /></PLayerLayout>} />
+      <Route path="/search" element={<PLayerLayout>search</PLayerLayout>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
